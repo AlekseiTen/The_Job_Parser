0803ba126bd7a9d2a5d7b83090cc98bf15306e51
Aleksei Ten <amailcn@mail.ru>
2024-08-14 00:18:47 +0300

    класс вакансий в удобном виде

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,15 +1,17 @@
 class Vacancy:
     """Класс для организации данных по вакансиям в удобном виде. хранит в себе полезные атрибуты по вакансиям"""
 
-    def __init__(self, id: str | int, name: str, alternate_url: str, salary: int, requirement: str, employer_id: str, employer_name: str):
+    def __init__(self, vacancy_id: str | int, vacancy_name: str, vacancy_url: str, salary: int, currency: int,
+                 requirement: str, employer_id: str, employer_name: str):
         """ Конструктор класса """
 
-        self.id = id
-        self.name = name
+        self.vacancy_id = vacancy_id
+        self.vacancy_name = vacancy_name
         self.employer_id = employer_id
         self.employer_name = employer_name
-        self.alternate_url = alternate_url
+        self.vacancy_url = vacancy_url
         self.salary = salary
+        self.currency = currency
         self.requirement = requirement
 
     def __lt__(self, other):
@@ -20,15 +22,17 @@ class Vacancy:
     def __str__(self):
         """ Строковое представление вакансии """
 
-        return (f'ID вакансии: {self.id}\n'
+        return (f'ID вакансии: {self.vacancy_id}\n'
                 f'ID компании: {self.employer_id}\n'
                 f'Наименование компании: {self.employer_name}\n'
-                f'Наименование вакансии: - {self.name}\n'
-                f'Ссылка на вакансию {self.alternate_url}\n'
+                f'Наименование вакансии: - {self.vacancy_name}\n'
+                f'Ссылка на вакансию {self.vacancy_url}\n'
                 f'Зарплата от - {self.salary},\n'
+                f'Валюта - {self.currency},\n'
                 f'Краткое описание: {self.requirement}\n')
 
 
 if __name__ == "__main__":
-    test_inst = Vacancy('12312', 'python', 'dasdasxvsda', 100, 'вообще пофиг', 740, 'яндекс')
+    test_inst = Vacancy('12312', 'python', 'dasdasxvsda', 100,
+                        'рубли', 'пофиг', '11', 'яндекс')
     print(test_inst)
