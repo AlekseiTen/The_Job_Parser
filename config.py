95737dd7eabeae732c2cb2b4e89cfbb09498d3a3
Aleksei Ten <amailcn@mail.ru>
2024-08-14 00:18:30 +0300

    путь до файла

--- config.py
+++ config.py
@@ -1,16 +1,3 @@
-from configparser import ConfigParser
-import psycopg2
+import os
 
-def config(filename="database.ini"):
-    '''коннектор для соединения с БД. при вызове можно передать другие аргументы '''
-    config = ConfigParser()
-    config.read(filename)
-    database_config = dict(config.items('database'))
-    conn = psycopg2.connect(
-        host=database_config['host'],
-        database=database_config['database'],
-        user=database_config['user'],
-        password=database_config['password'],
-        port=database_config['port'],
-    )
-    return conn
+ROOT_DIR = os.path.dirname(__file__)
